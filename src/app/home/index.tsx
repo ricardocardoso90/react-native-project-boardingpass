c3b75757f6808fa954a5d93efaea913ef4c16b3b
ricardocardoso90 <ricardomanolo90@gmail.com>
2024-09-26 14:47:07 -0300

    update commit

---
+++ src/app/home/index.tsx
@@ -0,0 +1,10 @@
+import { styles } from "./styles";
+import { Text, View } from "react-native";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá Mundão.</Text>
+    </View>
+  )
+};
