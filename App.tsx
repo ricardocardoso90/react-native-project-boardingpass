4f273da56111f2a616d4f0948ad37f4153806ddd
Ricardo Cardoso <ricardomanolo90@gmail.com>
2026-03-30 19:23:09 -0300

    update commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Home } from '@/app/home';
-import { useFonts, Poppins_400Regular, Poppins_700Bold, Poppins_500Medium, Poppins_800ExtraBold } from '@expo-google-fonts/poppins';
 import { StatusBar } from 'expo-status-bar';
+import { useFonts, Poppins_400Regular, Poppins_700Bold, Poppins_500Medium, Poppins_800ExtraBold } from '@expo-google-fonts/poppins';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_700Bold, Poppins_500Medium, Poppins_800ExtraBold });
